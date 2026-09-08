b2054196bab7a72c01d5d6d7e0bcca3c8e547420
SERAFINO <ymontenegro@estudiantec.cr>
2026-09-07 22:05:23 -0600

    fix: Se arregla el nombre del repositorio a publicar en gh pages, deploy listo

--- B2B-Front/vite.config.ts
+++ B2B-Front/vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: 'https://Seraf1n0.github.io/PY1_Comercio-Electronico-Catalogo-Base-B2B/',
+  base: 'https://Seraf1n0.github.io/Lab2-Ecommerce/',
 })
